60090e784d71777c8532f52a8c4283df325bb405
HugoR812 <149978691+HugoR812@users.noreply.github.com>
2026-01-14 10:25:41 -0500

    creation du test de la fonction redemarrer

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,22 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import app from '../../src/app';
+
+const request = require('supertest');
 
 describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+  
+  beforeAll( async () => {
+    await request(app).post('/api/v1/joueur').send({ nom: 'Joueur1' });
+    await request(app).post('/api/v1/joueur').send({ nom: 'Joueur2' });
   });
+
+  it("devrait implémenter test", async () => {
+    const response = await request(app).get('/api/v1/jeu/redemarrerJeu');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['content-type']).toMatch(/json/);
+  });
+
 });
